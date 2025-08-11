4ea36ced611e237335d1eaae575cf9dd9799c1f5
Nikolienas <nicholienass@gmail.com>
2025-08-12 03:39:52 +0500

    Config docker and nginx in project

---
+++ Makefile
@@ -0,0 +1,33 @@
+# Основные команды
+init: docker-down-clear docker-pull docker-build docker-up
+migrate-data: makemigrate migrate
+down: docker-down-clear
+restart: down init
+
+# Докер
+
+docker-up:
+	docker-compose up
+
+docker-down:
+	docker-compose down --remove-orphans
+
+docker-down-clear:
+	docker-compose down -v --remove-orphans
+
+docker-pull:
+	docker-compose pull
+
+docker-build:
+	docker-compose build
+
+# Джанго
+
+makemigrate:
+	docker exec django python manage.py makemigrations
+
+migrate:
+	docker exec django python manage.py migrate
+
+createsuperuser:
+	python $(PWD)/src/backend/manage.py createsuperuser
---
+++ src/backend/entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Путь к файлу-флагу для проверки загрузки фикстур
+FLAG_FILE=/app/fixtures_loaded.flag
+
+# Проверка на существование файл-флага
+if [ ! -f "$FLAG_FILE" ]; then
+    until cd /app
+    do
+        echo "Waiting for server volume..."
+    done
+
+    if [ "$DATABASE" = "postgres" ]; then
+        while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+            sleep 0.1
+        done
+        echo "POSTGRES READY"
+    fi
+
+    # Создание миграций
+    python manage.py makemigrations
+
+
+    # Миграция
+    python manage.py migrate
+
+    # Сбор статических файлов
+    python manage.py collectstatic --noinput
+
+    # Загрузка фикстур
+    # ./manage.py loaddata fixtures/User_Custom_User.json --app User_Custom_User
+
+    # Создание файл-флага, указывающий на то, что фикстуры были загружены
+    touch $FLAG_FILE
+fi
+
+# Запуск сервера Django
+nohup python manage.py runserver 0.0.0.0:8000
